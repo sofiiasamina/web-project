c650bac634eb44b12018d9ad0669c783872d530b
Sofiia Samina <sophiesamina@gmail.com>
2020-03-30 17:22:58 +0300

    second commit

---
+++ js/main.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+    let tabsItem = $('.header-tab-item');
+
+    tabsItem.on('click', function(event) {
+        event.preventDefault();
+        let activeContent = $(this).attr('href');
+        $('.visible').toggleClass('visible');
+        $(activeContent).toggleClass('visible');
+        $('.tab-item-active').toggleClass('tab-item-active');
+        $(this).toggleClass('tab-item-active');
+    });
+
+});
