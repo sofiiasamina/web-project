4bf2f73a9a0e5cc1d6cf7c6d48ac66a1319dc631
Sofiia Samina <sophiesamina@gmail.com>
2020-04-07 18:54:08 +0300

    5th commit

--- js/main.js
+++ js/main.js
@@ -1,4 +1,33 @@
+const cards = document.querySelectorAll('.card');
+
+for (let i = 0; i < cards.length; i++) {
+    const card = cards[i];
+    card.addEventListener('mousemove', startRotate);
+    card.addEventListener('mouseout', stopRotate);
+}
+
+function startRotate(event){
+    const cardItem = this.querySelector('.card-item');
+    const halfHeight = cardItem.offsetHeight/2;
+    const halfWidth = cardItem.offsetWidth/2;
+    cardItem.style.transform = 'rotateX('+-(event.offsetY - halfHeight)/5 +'deg) rotateY('+(event.offsetX - halfWidth)/5 + 'deg)'; 
+}
+
+function stopRotate(event){
+    const cardItem = this.querySelector('.card-item');
+    cardItem.style.transform = 'rotateX(0) rotateY(0)';
+}
+$(document).ready(function(){
+    $('.wrapper').addClass('active');
+    
+
+    var scene = document.getElementById('parallax');
+    var parallaxInstance = new Parallax(scene);
+});
+
 $(document).ready(function(){ //переключалка між табами About, Actor, Abilities
+
+    
     let tabsItem = $('.header-tab-item');
 
     tabsItem.on('click', function(event) {
